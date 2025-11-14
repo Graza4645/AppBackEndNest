c07514c1ee212516effe33e4e62d352e08423acf
itnoushad3-boop <itnoushad3@gmail.com>
2025-11-14 12:28:21 +0530

    C:/Users/NoushadA/AppData/Local/Programs/Git/

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-     app.useGlobalPipes(new ValidationPipe({
+const server = express();
+
+const createNestServer = async (expressInstance) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+  
+  app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: false,
     transform: true,
   }));
-  app.setGlobalPrefix('api/v1')
-   app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
-}
-bootstrap();
+  
+  app.setGlobalPrefix('api/v1');
+  app.enableCors();
+  
+  return app.init();
+};
+
+createNestServer(server)
+  .then(() => console.log('Nest Ready'))
+  .catch(err => console.error('Nest broken', err));
+
+export default server;
