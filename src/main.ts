37b2daf02ff31fc42c877e4ced3807f630d673af
itnoushad3-boop <itnoushad3@gmail.com>
2025-11-16 00:15:21 +0530

    C:/Users/NoushadA/AppData/Local/Programs/Git/

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express';
 
-const server = express();
-
-const createNestServer = async (expressInstance) => {
-  const app = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(expressInstance),
-  );
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
   
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
@@ -20,17 +13,10 @@ const createNestServer = async (expressInstance) => {
   app.setGlobalPrefix('api/v1');
   app.enableCors();
   
-  return app.init();
-};
+  const port = process.env.PORT || 8000;
+  await app.listen(port);
+  console.log('Nest Ready');
+  console.log(`🚀 Server running on port ${port}`);
+}
 
-createNestServer(server)
-  .then(() => {
-    const port = process.env.PORT || 8000;
-    server.listen(port, () => {
-      console.log('Nest Ready');
-      console.log(`🚀 Server running on http://localhost:${port}`);
-    });
-  })
-  .catch(err => console.error('Nest broken', err));
-
-export default server;
+bootstrap().catch(err => console.error('Nest broken', err));
